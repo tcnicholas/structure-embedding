9a2ef3bebbaa4cc422539f4e15fd56d80b188fbf
Thomas Nicholas <tcnicholas@me.com>
2025-03-10 11:16:48 +0100

    bump 0.0.2 -> 0.0.3

--- src/structure_embedding/__init__.py
+++ src/structure_embedding/__init__.py
@@ -2,4 +2,4 @@ from structure_embedding.data import Dataset
 from structure_embedding.model import StructureEmbedding
 
 __all__ = ["Dataset", "StructureEmbedding"]
-__version__ = "0.0.2"
+__version__ = "0.0.3"
