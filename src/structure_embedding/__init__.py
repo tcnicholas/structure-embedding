c38dbda272bda788210b3408aa36c4e62aeafec8
Thomas Nicholas <tcnicholas@me.com>
2025-03-10 19:13:26 +0100

    bump 0.0.5 -> 0.0.6

--- src/structure_embedding/__init__.py
+++ src/structure_embedding/__init__.py
@@ -2,4 +2,4 @@ from structure_embedding.data import Dataset
 from structure_embedding.transformer import Transformer
 
 __all__ = ["Dataset", "Transformer"]
-__version__ = "0.0.5"
+__version__ = "0.0.6"
